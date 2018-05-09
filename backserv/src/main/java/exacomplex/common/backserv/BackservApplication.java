421551a22d0dda80f31c65d82453552db5c196b0
sunwj326 <sunwj326@hotmail.com>
2018-05-09 17:58:37 +0800

    second commit

---
+++ backserv/src/main/java/exacomplex/common/backserv/BackservApplication.java
@@ -0,0 +1,12 @@
+package exacomplex.common.backserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackservApplication.class, args);
+	}
+}
